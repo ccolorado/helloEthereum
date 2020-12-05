6df4ae81d73ad834c2d3fee39f5667bb4555ae4f
Carlos <carlos.colorado@gmail.com>
2020-12-04 21:26:51 -0600

    Adds test and dependencies

--- test/yam_token.js
+++ test/yam_token.js
@@ -1,14 +1,102 @@
-const YamToken = artifacts.require("YamToken");
 const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("YamToken", function (/* accounts */) {
-  it("should assert true", async function () {
-    await YamToken.deployed();
-    return assert.isTrue(true);
-  });
+const YamToken = artifacts.require("YamToken");
+const YamTokenContract = require('../build/contracts/YamToken.json');
+
+const BigNumber = require('bignumber.js');
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+contract("YamToken", function (accounts) {
+  beforeEach(async function () {
+    this.tokenName = 'YamToken';
+    this.tokenSymbol = 'YT';
+
+    this.yamToken = await YamToken.new(
+      this.tokenName, this.tokenSymbol
+    );
+
+    this.CYamToken = await new web3.eth.Contract(
+      YamTokenContract.abi, this.yamToken.address
+    );
+
+
+  })
+
+  describe('Token integrity', async function () {
+
+    it('have a totalSupply function', async function () {
+
+      const _totalSupply = await this.CYamToken.methods.totalSupply().call()
+      _totalSupply.should.be.equal('0');
+
+    })
+
+    it('have a balanceOf function', async function () {
+
+      const _balanceOf = await this.CYamToken.methods.balanceOf(accounts[0]).call()
+      _balanceOf.should.be.equal('0');
+
+    })
+
+
+    it('have a allowance function', async function () {
+      const _allowance = await this.CYamToken.methods.allowance(accounts[0], accounts[1]).call()
+      _allowance.should.be.equal('0');
+    })
+
+
+    it('have a transfer function', async function () {
+
+      const _txReciept = await this.CYamToken.methods.transfer(
+        accounts[0], BigNumber('1000000000000000000')
+      ).send( { from:accounts[0] }).should.be.rejectedWith(
+        Error,
+        'revert ERC20: transfer amount exceeds balance'
+      )
+
+    })
+
+  })
+
+  describe('event emmision', async function () {
+    beforeEach(async function () {
+
+    })
+
+    it('emit a Transfer event when minting', async function () {
+
+      await this.CYamToken.methods.mint(accounts[0], BigNumber('1000000000000000000')).send({
+        from: accounts[0],
+        gas: 6721975
+      }).should.be.fulfilled
+
+      const events = await this.CYamToken.getPastEvents('Transfer');
+
+      events[0].returnValues.to.should.be.equal(accounts[0])
+      events[0].returnValues.value.should.be.equal('1000000000000000000')
+
+    })
+
+    it('emit a Approval event when minting', async function () {
+
+      await this.CYamToken.methods.approve(accounts[2], BigNumber('1000000000000000000')).send({
+        from: accounts[0],
+        gas: 6721975
+      }).should.be.fulfilled
+
+      const events = await this.CYamToken.getPastEvents('Approval');
+
+      events[0].returnValues.owner.should.be.equal(accounts[0])
+      events[0].returnValues.spender.should.be.equal(accounts[2])
+      events[0].returnValues.value.should.be.equal('1000000000000000000')
+
+    })
+
+  })
+
 });
